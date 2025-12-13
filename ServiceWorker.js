aa70b05567a3daaef2e6cc262f7d1b8615483ef0
Yuanyuzhou123-create <17741228184@163.com>
2025-12-13 22:51:45 +0800

    Add files via upload

---
+++ ServiceWorker.js
@@ -0,0 +1,33 @@
+const cacheName = "DefaultCompany-YanKe-0.1";
+const contentToCache = [
+    "Build/新建文件夹 (2).loader.js",
+    "Build/新建文件夹 (2).framework.js.unityweb",
+    "Build/新建文件夹 (2).data.unityweb",
+    "Build/新建文件夹 (2).wasm.unityweb",
+    "TemplateData/style.css"
+
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
